72599f78ad52bea468073b2f97a6717ed2165b9c
Tin Anh Nguyen <anhtin95@gmail.com>
2019-06-25 19:22:53 +0200

    Updates

---
+++ precache-manifest.d83919acc176097d441904144f9cfa9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8a837999b3f65b232a208cf2127670",
+    "url": "/bodhi-calendar-v2/index.html"
+  },
+  {
+    "revision": "b7aa4cf450b2065b3aa8",
+    "url": "/bodhi-calendar-v2/static/css/main.21f1f725.chunk.css"
+  },
+  {
+    "revision": "74ae43ccfeb1c056eed1",
+    "url": "/bodhi-calendar-v2/static/js/2.8d38264b.chunk.js"
+  },
+  {
+    "revision": "b7aa4cf450b2065b3aa8",
+    "url": "/bodhi-calendar-v2/static/js/main.01e50983.chunk.js"
+  },
+  {
+    "revision": "76923c8f563565e73b4f",
+    "url": "/bodhi-calendar-v2/static/js/runtime~main.b71fb365.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bodhi-calendar-v2/precache-manifest.192130a484950d960ef769b8b88098d3.js"
+  "/bodhi-calendar-v2/precache-manifest.d83919acc176097d441904144f9cfa9e.js"
 );
 
 self.addEventListener('message', (event) => {
