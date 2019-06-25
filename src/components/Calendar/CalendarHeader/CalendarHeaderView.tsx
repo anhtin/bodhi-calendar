69ebd3cb0267853d9687a3e9b668494a9f6c9d66
Tin Anh Nguyen <anhtin95@gmail.com>
2019-06-25 16:07:41 +0200

    Change button size and alignment in header

--- src/components/Calendar/CalendarHeader/CalendarHeaderView.tsx
+++ src/components/Calendar/CalendarHeader/CalendarHeaderView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { DateTime } from 'luxon';
-import { Button, Title, Subtitle } from 'bloomer';
+import { Button } from 'bloomer';
 
 import Flex from 'components/Flex';
 
@@ -18,13 +18,13 @@ export const CalendarHeaderView = ({
   const month = displayDate.monthLong;
   const year = displayDate.year;
   return (
-    <Flex>
-        <Button onClick={onPrevMonth}>&lt;</Button>
+    <Flex alignItems="center">
+        <Button onClick={onPrevMonth} isSize="large">&lt;</Button>
         <Flex direction="column" flex={1} alignItems="center">
           <h2>{year}</h2>
           <h1 style={{ fontSize: '1.5em', fontWeight: 'bold' }}>{month}</h1>
         </Flex>
-        <Button onClick={onNextMonth}>&gt;</Button>
+        <Button onClick={onNextMonth} isSize="large">&gt;</Button>
     </Flex>
   );
 };
