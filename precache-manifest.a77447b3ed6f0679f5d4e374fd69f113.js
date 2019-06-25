a37636063f95fec14acce69c52a3257ac94b007f
Tin Anh Nguyen <anhtin95@gmail.com>
2019-06-25 16:00:48 +0200

    Updates

---
+++ precache-manifest.a77447b3ed6f0679f5d4e374fd69f113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1f656ca957b8ec76e542bb8e65a852",
+    "url": "/bodhi-calendar-v2/index.html"
+  },
+  {
+    "revision": "5a617abf91d69548586d",
+    "url": "/bodhi-calendar-v2/static/css/2.2687cd84.chunk.css"
+  },
+  {
+    "revision": "a845802090844a95d885",
+    "url": "/bodhi-calendar-v2/static/css/main.6cfcd663.chunk.css"
+  },
+  {
+    "revision": "5a617abf91d69548586d",
+    "url": "/bodhi-calendar-v2/static/js/2.e3911c73.chunk.js"
+  },
+  {
+    "revision": "a845802090844a95d885",
+    "url": "/bodhi-calendar-v2/static/js/main.c5d11673.chunk.js"
+  },
+  {
+    "revision": "76923c8f563565e73b4f",
+    "url": "/bodhi-calendar-v2/static/js/runtime~main.b71fb365.js"
+  }
+]);
