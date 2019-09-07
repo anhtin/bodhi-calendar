4cef7143d80c352c2f5f151f46a3ff4f113d5631
Tin Anh Nguyen <anhtin95@gmail.com>
2019-09-07 17:02:15 +0200

    Add icon and credits

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,20 +3,54 @@ import React from 'react';
 import Calendar from 'components/Calendar';
 import { SettingsProvider } from 'components/context/SettingsContext';
 import { DateProvider } from 'components/context/DateContext';
-import styles from './App.module.css';
+import logo from 'assets/images/logo.svg';
+import './App.css';
 
 import 'assets/sass/main.scss';
 
 const App = () => {
   return (
-    <div className={styles.app}>
+    <div className="app">
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <main>
       <SettingsProvider>
         <DateProvider>
           <Calendar />
         </DateProvider>
       </SettingsProvider>
-    </div>
+    </main>
   );
 }
+
+function Footer() {
+  return (
+    <footer>
+      <img
+        className="logo"
+        alt="Logo with a bodhi leaf"
+        src={logo}
+      />
+      <div className="credits">
+        <p>
+          <em>Bodhi Calendar</em> is an
+          {' '}
+          <a href="https://github.com/anhtin/bodhi-calendar-v2">
+            open-source project
+          </a>
+          {' '}created by
+          {' '}
+          <a href="https://github.com/anhtin">Tin Anh Nguyen</a>.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default App;
