918002a1aaac86fdbdddcc106c8f733f4f0898a4
Tin Anh Nguyen <anhtin95@gmail.com>
2021-01-01 02:37:06 +0100

    Add fallback locale for date formatting
    We should probably look into lazily loading the desired locale in the
    future.

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,16 +1,19 @@
 import { format, formatISO, parseISO, startOfWeek } from 'date-fns';
-import { getUserLocale } from 'get-user-locale';
+import enUs from 'date-fns/locale/en-US';
+import nb from 'date-fns/locale/nb';
+import { getUserLocales } from 'get-user-locale';
 
-const userLocale = getUserLocale();
-
-const locale = () => require(`date-fns/locale/${userLocale}`).default;
+const NO_LOCALES = ['no', 'nb', 'nn'];
+const locale = getUserLocales().some((locale) => NO_LOCALES.includes(locale))
+  ? nb
+  : enUs;
 
 export const WEEK_DAYS: Array<string> = [0, 1, 2, 3, 4, 5, 6].map((day) =>
-  locale().localize.day(day, { width: 'abbreviated' })
+  locale.localize?.day(day, { width: 'abbreviated' })
 );
 
 export function formatDate(date: Date, pattern: string): string {
-  return format(date, pattern, { locale: locale() });
+  return format(date, pattern, { locale });
 }
 
 export function formatISODate(date: Date): string {
@@ -22,5 +25,5 @@ export function parseISODate(date: string): Date {
 }
 
 export function getStartOfWeek(date: Date) {
-  return startOfWeek(date, { locale: locale() });
+  return startOfWeek(date, { locale });
 }
