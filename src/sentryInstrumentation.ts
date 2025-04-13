2315059825225a2a7452360000a8fa53a148e633
Nguyen, Tin Anh <anhtin95@gmail.com>
2025-04-13 20:06:16 +0200

    Fix broken development build

--- src/sentryInstrumentation.ts
+++ src/sentryInstrumentation.ts
@@ -1,7 +1,9 @@
 import * as Sentry from '@sentry/react';
 
-Sentry.init({
-  dsn: 'https://db09e878f14748c5ac6e6c24b94b78ca@sentry.io/1723548',
-  release: process.env.VITE_SENTRY_RELEASE,
-  integrations: [],
-});
+if (!import.meta.env.DEV) {
+  Sentry.init({
+    dsn: 'https://db09e878f14748c5ac6e6c24b94b78ca@sentry.io/1723548',
+    release: process.env.VITE_SENTRY_RELEASE,
+    integrations: [],
+  });
+}
